122efb627aa30d26703fb92671d8b00cb9d35ee4
Arnob <nswarnob@gmail.com>
2025-11-18 02:16:02 +0600

    first problem solved at type script

---
+++ solution.js
@@ -0,0 +1,16 @@
+//Create a function formatValue that accepts a value which may be a string, number, or boolean, and returns the following based on the value type:
+// If the input is a string → return the string in uppercase
+// If the input is a number → return the number multiplied by 10
+// If the input is a boolean → return the opposite value (true → false, false → true)
+function formatValue(value) {
+    if (typeof value === "string") {
+        return value.toUpperCase();
+    }
+    else if (typeof value === 'number') {
+        return value * 10;
+    }
+    else if (typeof value === 'boolean') {
+        return !value;
+    }
+}
+console.log(formatValue("hello"));
